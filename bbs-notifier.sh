640c2332e31fa4511e9c2a8f029aa8acd1ba8c86
Chunyang Xu <xuchunyang56@gmail.com>
2014-08-08 12:22:41 +0800

    Add basic sh sketch

--- bbs-notifier.sh
+++ bbs-notifier.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env bash
 
 #----------------------------------------------------
-
-SBBS_API_URL="http://bbs.seu.edu.cn/api/"
-
 USER_NAME="xcy"
 USER_PASSWORD="xxx"
 USER_EMAIL="xuchunyang56@gmail.com"
 
+#----------------------------------------------------
+SBBS_API_URL="http://bbs.seu.edu.cn/api/"
+
+# TODO check return status
+STATUS="false"
 # 1. get token
 token=$(curl -s --data "user=${USER_NAME}&pass=${USER_PASSWORD}" ${SBBS_API_URL}token.json | jq ".token")
 
-# my token is eGN5:==QfMFZSD71CUxQ2tKzcbZmw
-
 # 2. get and notification and parse
 ## There are 3 types notifications, "mails", "ats" and "replies"
-curl -s --data "token=${token}" http://bbs.seu.edu.cn/api/notifications.json | jq '.mails'
+MAIL=$(curl -s --data "token=${token}" http://bbs.seu.edu.cn/api/notifications.json | jq '.mails')
+
+# TODO Add other two fields
 
 # 4. send email
-echo "unread notifications" | mail -s "[BBS通知]" $USER_EMAIL
+if [[ "$MAIL" == "null" ]]; then
+	echo "no any notifications"
+else
+	echo "$MAIL" | mail -s "[BBS通知]" $USER_EMAIL
+fi
